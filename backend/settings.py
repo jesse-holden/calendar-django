9a8ce6bcd2295e831ae2a27003493bbe1840f7c2
jesse-holden <jesse.holden@cyberfiend.com>
2019-02-18 22:43:00 -0500

    update for heroku

--- backend/settings.py
+++ backend/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from calendarapp import views
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'calendar', views.CalendarItemView, 'calendarapp')
 
 urlpatterns = [
-    path('', views.index ),path('admin/', admin.site.urls),         path('api/', include(router.urls))
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
