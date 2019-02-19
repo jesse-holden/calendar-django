2874e552a2267d2987ca96dc7ff7ec753a97c7c6
jesse-holden <jesse.holden@cyberfiend.com>
2019-02-19 02:20:50 -0500

    add second heroku host

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9wfjir%6$2wca1vy%ztfpofsq^vii3mmf5kds_+yt9_l17)9ax'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'calendar-django-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'calendar-django-app.herokuapp.com', 'oxcorp-calendar-app.herokuapp.com']
 
 
 # Application definition
